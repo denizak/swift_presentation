e2a40a2588ad064982471ebb05841fb4298df965
Deni Zakya <denizakya@gmail.com>
2017-08-25 23:07:18 +0700

    Add Version

--- SwiftPresentation.playground/Pages/Community.xcplaygroundpage/Contents.swift
+++ SwiftPresentation.playground/Pages/Community.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
-//:Swift Community
-/*
+/*:
+ ## Swift Community
  * swift mailing list : https://swift.org/community/#community-structure
  * slack : swift-lang.slack.com
  * telegram group : https://telegram.me/swiftID
  */
+//: [⬅️](@previous) [➡️](@next)
